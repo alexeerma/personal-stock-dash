4dc910ff87f0e28aa20b27499fe6066ae3fce074
alexeerma <aleksander.eerma@hotmail.com>
2023-07-18 15:17:43 +0300

    Initial data for starting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      borderWidth: {
+        1: "1px",
+
+      },
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
